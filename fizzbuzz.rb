3ecb23227c892e09dfbd5782d7d1242ed20f7cda
Shadman Ahmed <shadman@outlook.com>
2020-03-30 03:18:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,13 @@
 def fizzbuzz(int)
   if int % 5 == 0
     "Buzz"
-  end
-  if int % 3 == 0
+
+  elsif int % 3 == 0
     "Fizz"
-  end
-  if int % 3 == 0 && int % 5 == 0
+
+  elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
+
+  else nil
   end
 end
