94ae28d8a47de3646fe08213de228c6254256f46
Shadman Ahmed <shadman@outlook.com>
2020-03-30 02:34:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0
     "Fizz"
+elseif int % 5 == 0
+    "Buzz"
+elseif int % 5 && 3
+    "FizzBuzz"
   end
-
-  
+  end
